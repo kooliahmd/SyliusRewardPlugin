0c1962a3a86ad12c13f3edf6ddeaa9d7cb97591b
Ahmed Kooli <kooliahmd@gmail.com>
2017-11-05 06:47:05 +0200

    available rp checker

--- src/Validator/Constraints/UsedRewardPointsNumberValidator.php
+++ src/Validator/Constraints/UsedRewardPointsNumberValidator.php
@@ -11,7 +11,9 @@ declare(strict_types=1);
 
 namespace SnakeTn\Reward\Validator\Constraints;
 
+use SnakeTn\Reward\AvailableRewardPointsChecker\AvailableRewardPointsChecker;
 use SnakeTn\Reward\CustomerEligibilityChecker\CustomerEligibilityCheckerInterface;
+use SnakeTn\Reward\Entity\Order;
 use Sylius\Component\Core\Model\OrderInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
@@ -19,20 +21,25 @@ use Symfony\Component\Validator\ConstraintValidator;
 class UsedRewardPointsNumberValidator extends ConstraintValidator
 {
     private $customerEligibilityChecker;
+    private $availableRewardPointsChecker;
 
-    public function __construct(CustomerEligibilityCheckerInterface $customerEligibilityChecker)
-    {
+    public function __construct(
+        CustomerEligibilityCheckerInterface $customerEligibilityChecker,
+        AvailableRewardPointsChecker $availableRewardPointsChecker
+    ) {
         $this->customerEligibilityChecker = $customerEligibilityChecker;
+        $this->availableRewardPointsChecker = $availableRewardPointsChecker;
     }
 
     public function validate($order, Constraint $constraint)
     {
-        /**@var $order OrderInterface */
-
+        /**@var $order Order */
         if (!$order->getCustomer()) {
             $this->context->addViolation("reward.used_reward_points.customer_is_missing");
         } elseif (!$this->customerEligibilityChecker->check($order->getCustomer())) {
             $this->context->addViolation("reward.used_reward_points.customer_not_eligible");
+        } elseif (!$this->availableRewardPointsChecker->check($order->getCustomer(), $order->getUsedRewardPointMovement())) {
+            $this->context->addViolation("reward.used_reward_points.insufficient_available_reward_points");
         }
 
     }
